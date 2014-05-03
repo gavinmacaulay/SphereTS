a05f7102e56768140ee6526f9e439caa0435f7b3
Gavin Macaulay <gavin@macaulay.co.nz>
2014-05-03 21:01:25 +0200

    Improved doc string Added argument checking Started on a calling function for sphereTS

--- sphereTS.py
+++ sphereTS.py
@@ -9,26 +9,49 @@ import math
 import cmath
 from scipy import special
 
+from numpy import arange
+
 def sphereTS(f, a, c, c1, c2, rho, rho1):
     """
-    Calculates the backscattered acoustic TS of an elastic sphere in a fluid.
+    Calculates the acoustic backscattered target strength of an elastic sphere immersed in a fluid.
     
-    Calculates the TS as defined by equations 6 to 9 in: 
-    MacLennan, D.N., 1981. The Theory of Solid Spheres 
-    as Sonar Calibration Targets. 22, Department of 
-    Agriculture and Fisheries for Scotland. 
+    Implements equations 6 to 9 in MacLennan (1981). 
     
-    Input variables are:
-    f - acoustic frequency, [Hz]
-    a - sphere radius [m]
-    c - sound speed in water [m/s]
-    c1 - longitudinal sound speed of the sphere material [m/s]
-    c2 - transverse sound speed of the sphere material [m/s]
-    rho - density of fluid that surrounds the sphere [kg/m^3]
-    rho1 - density of sphere material [kg/m^3]
-     
-    The input variables should all be scalars.
+    Args:
+        :f: acoustic frequency [Hz]
+        :a: sphere radius [m]
+        :c: sound speed in the fluid that surrounds the sphere [m/s]
+        :c1: longitudinal sound speed of the sphere material [m/s]
+        :c2: transverse sound speed of the sphere material [m/s]
+        :rho: density of the fluid that surrounds the sphere [kg/m^3]
+        :rho1: density of the sphere material [kg/m^3]
+
+    Returns:
+        The target strength of the sphere [dB re 1m^2]
+      
+    Raises:
+        ValueError if any of the input arguments are less than zero.        
+    
+    References:
+        MacLennan, D.N., 1981. The Theory of Solid Spheres 
+        as Sonar Calibration Targets. Scottish Fisheries Research 
+        Report No. 22, Department of Agriculture and Fisheries for Scotland. 
     """
+    
+    if f < 0:  
+        raise ValueError("Frequency must be greater than 0 (was {}).".format(f))
+    if a < 0:
+        raise ValueError('Sphere radius must be greater than 0 (was {}).'.format(a))
+    if c < 0:
+        raise ValueError('Fluid sound speed must be greater than 0 (was {})'.format(c))
+    if c1 < 0:
+        raise ValueError('Longitudinal sound speed of the sphere must be greater than 0 (was {})'.format(c1))
+    if c2 < 0:
+        raise ValueError('Transverse sound speed of the sphere must be greater than 0 (was {})'.format(c2))
+    if rho < 0:
+        raise ValueError('Density of the fluid must be greater than 0 (was {})'.format(rho))
+    if rho1 < 0:
+        raise ValueError('Density of the sphere must be greater than 0 (was {})'.format(rho1))
     
     k = 2.0*math.pi*f/c
     q = k*a
@@ -78,3 +101,21 @@ def sphereTS(f, a, c, c1, c2, rho, rho1):
     form = -2.0/q*form
     sigma = math.pi * a**2 * abs(form)**2
     return 10.0 * math.log10(sigma / (4.0*math.pi))
+
+def sphereTSFreqResponse():
+    """
+    
+    """
+    a = 0.0381/2
+    c = 1490
+    c1 = 6853
+    c2 = 4171
+    rho = 1026.2
+    rho1 = 14900
+    fstart = 12e3
+    fstop = 200e3
+    
+    for f in arange(fstart, fstop, 100):
+        ts = sphereTS(f, a, c, c1, c2, rho, rho1)
+        print('TS = {} at {} kHz.'.format(ts, f/1000.))
+    pass
