51ba33075d600ef858495fd18b3eb7518642da95
Gavin Macaulay <gavin@macaulay.co.nz>
2014-05-06 21:43:21 +0200

    Start to implement bandwidth averaging Implement user defined sphere material remove unused code make display of TS at spot freqs nicer

--- sphereTSGUI.py
+++ sphereTSGUI.py
@@ -19,10 +19,10 @@ class uiHandler(Handler):
     """
     """
     def calculate(self, info):
-        material = info.object.sphere_material
-        
-        params = {'fstart': info.object.freq_start*1e3,
-                  'fstop': info.object.freq_end*1e3,
+
+        half_bw = info.object.averaging_bandwidth/2.0
+        params = {'fstart': (info.object.freq_start-half_bw)*1e3,
+                  'fstop': (info.object.freq_end+half_bw)*1e3,
                   'fstep': info.object.freq_step*1e3,
                   'a': info.object.sphere_diameter/2000.0,
                   'rho1': info.object.sphere_density,
@@ -32,13 +32,22 @@ class uiHandler(Handler):
                   'rho': info.object.fluid_density}
 
         f, TS = sphereTSFreqResponse(**params)
-                                     
+                       
+        #fsubset = f[f>=info.object.freq_start*1e3 && f<=info.object.freq_end*1e3]
+        #TSsubset = TS[f>=info.object.freq_start*1e3 && f<=info.object.freq_end*1e3]
         plt.figure()
         plt.plot(f/1e3, TS)
         plt.xlabel('Frequency (kHz)')
         plt.ylabel('TS (dB re 1 m$^2$)')
         plt.grid()
-        plt.title('{} sphere, {} mm'.format(material, params['a']*2*1000))
+
+        if info.object.use_user_material:
+            title_text = 'User defined sphere, {} mm'.format(params['a']*2000)
+        else:
+            title_text = '{} sphere, {} mm'.format(info.object.sphere_material, 
+                                                   params['a']*2000)
+
+        plt.title(title_text)
         
         # Then do the spot frequencies
         del params['fstart']
@@ -49,11 +58,11 @@ class uiHandler(Handler):
         freqs = list(set(info.object.spot_freqs+info.object.extra_spot_freqs))
         freqs.sort()
 
-        spot_TS_text = '  f (kHz)     TS (dB)'
+        spot_TS_text = 'f (kHz)     TS (dB)'
         for f in freqs:
             TS = sphereTS(f*1000, **params)
             plt.plot(f, TS, 'o')
-            spot_TS_text = spot_TS_text + '\n  {:>8g}     {:>8.1f}'.format(f, TS)
+            spot_TS_text = spot_TS_text + '\n{:>8g}     {:>8.1f}'.format(f, TS)
         
         ax = plt.gca()
         ylim = ax.get_ylim()
@@ -61,15 +70,19 @@ class uiHandler(Handler):
         text_x_pos = (xlim[1] - xlim[0])*0.05 + xlim[0]
         text_y_pos = (ylim[1] - ylim[0])*0.05 + ylim[0]
         if len(freqs) > 0:
-            plt.text(text_x_pos, text_y_pos, spot_TS_text, verticalalignment='bottom',
+            plt.text(text_x_pos, text_y_pos, spot_TS_text, 
+                     verticalalignment='bottom',
                      horizontalalignment='left',
-                     bbox=dict(facecolor=(.7, .7, .7), alpha=0.5, edgecolor='None'))
+                     bbox=dict(boxstyle='round,pad=0.5', 
+                               facecolor=(.7, .7, .7), 
+                               alpha=0.5, edgecolor='None'))
         plt.show()
     
     def object_sphere_material_changed(self, info):
         """
         """
         m = materialProperties()
+
         s = m[info.object.sphere_material]
         info.object.sphere_density = s['rho1']
         info.object.sphere_c1 = s['c1']
@@ -102,15 +115,8 @@ class uiHandler(Handler):
         return True
         
 # TODO
-# when the sphere properties are changed, deselect the selected material properties and change the plot title
 #
 # Limits on the input water temp, salinity, and depth numbers to sensible values
-#
-# Have multiple graph windows
-#
-# Print out the TS at spot frequencies
-#
-# Allow the specification of bandwidth for TS averaging
 #
 # Implement TS averaging over a bandwidth
 #
@@ -128,8 +134,7 @@ class sphereTSGUI(HasTraits):
     """
     Calculate and show the sphere TS using a GUI
     """
-
-       
+     
     default_material = 'Tungsten carbide'
     m = materialProperties()
     params = m[default_material]
@@ -161,6 +166,7 @@ class sphereTSGUI(HasTraits):
                       label='Transverse sound speed [m/s]')
 
     use_ctd = Bool(True)
+    use_user_material = Bool(False)
     
     fluid_c = Float(params['c'],
                     desc='Sound speed in water [m/s]',
@@ -176,16 +182,21 @@ class sphereTSGUI(HasTraits):
     freq_start = Float(12., label='Start frequency [kHz]')
     freq_end = Float(200., label='End frequency [kHz]')
     freq_step = Float(1, label='Step frequency [kHz]')
+    
+    averaging_bandwidth = Float(2.5, 
+                                label='Bandwidth for averaged TS [kHz]')
 
     CalculateButton = Action(name = 'Calculate', action = 'calculate')    
 
     view = View(Group(Group(
-                        Item('sphere_material', style='custom', 
-                             editor=EnumEditor(values=m.keys(), cols=2)), 
                         Item('sphere_diameter'),
-                        Item('sphere_density'),
-                        Item('sphere_c1'),
-                        Item('sphere_c2'), 
+                        Item('sphere_material', style='custom', 
+                             enabled_when = 'not use_user_material',
+                             editor=EnumEditor(values=m.keys(), cols=2)), 
+                        Item('use_user_material', label='Another material'),
+                        Item('sphere_density', enabled_when = 'use_user_material'),
+                        Item('sphere_c1', enabled_when = 'use_user_material'),
+                        Item('sphere_c2', enabled_when = 'use_user_material'), 
                         label='Sphere properties', show_border=True),
                         '10', # some extra space
                       Group(
@@ -195,7 +206,7 @@ class sphereTSGUI(HasTraits):
                         Item('fluid_depth', enabled_when = 'use_ctd'),
                         Item('fluid_c', enabled_when = 'not use_ctd', format_str='%.2f'),
                         Item('fluid_density', enabled_when = 'not use_ctd',format_str='%.2f'),
-                        label='Environment properties', show_border=True),
+                        label='Environmental properties', show_border=True),
                         '10', # some extra space
                       Group(
                         Item('spot_freqs', style='custom',
@@ -205,6 +216,7 @@ class sphereTSGUI(HasTraits):
                         Item('freq_start'),
                         Item('freq_end'),
                         Item('freq_step'),
+                        Item('averaging_bandwidth'),
                         label='Frequencies', show_border=True)),
             resizable=True, 
             title='Sphere TS calculator',
@@ -213,27 +225,3 @@ class sphereTSGUI(HasTraits):
 
 ts = sphereTSGUI()
 ts.configure_traits()
-
-
-def testCall():
-    """
-    
-    """
-    
-    #params = [0.0381/2, 1490, 6853, 4171, 1026.2, 14900]
-    #f, TS = sphereTSFreqResponse(12e3,200e3, *params, fstep=200)
-
-    m = ts.materialProperties()
-    params = m['Tungsten carbide']
-    
-    params['a'] = 0.0381/2.0
-    params['rho'] = 1026.2
-    params['c'] = 1490.0
-    
-    f, TS = sphereTSFreqResponse(12e3, 200e3, fstep=200, **params)    
-    
-    plt.clf()
-    plt.plot(f/1e3, TS)
-    plt.xlabel('Frequency (kHz)')
-    plt.ylabel('TS (dB re 1 m$^2$)')
-    plt.show()
