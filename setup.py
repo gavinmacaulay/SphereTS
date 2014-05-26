67850da49f942a1fa52128e1e2a0145ee3e15b48
Gavin Macaulay <gavin@macaulay.co.nz>
2014-05-26 15:49:41 +0200

    Add files to support the setup.py method of packagin this module

---
+++ setup.py
@@ -0,0 +1,39 @@
+"""    
+    XXX
+
+    Copyright 2014 Gavin Macaulay
+
+    This file is part of SphereTS.
+
+    SphereTS is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    SphereTS is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with SphereTS.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from distutils.core import setup
+
+setup(
+    name = 'sphereTS',
+    version = '1.0',
+    description = 'Description text goes here',
+    long_description = '',
+    author = 'Gavin Macaulay', 
+    author_email = 'gavin@macaulay.co.nz',
+    packages = '.',
+    url = 'https://bitbucket.org/gjm/spherets',
+    classifiers = ('Development Status :: 4 - Beta',
+                   'Intended Audience :: Science/Research',
+                   'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Topic :: Scientific/Engineering'),
+    )  
